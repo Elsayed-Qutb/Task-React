5b41ca83745d747a3adc9742e938f2811feb55ee
Elsayed Qutb <13elsyd@gmail.com>
2026-04-05 12:00:32 +0200

    fix image paths for GitHub Pages

--- src/Mainheader.jsx
+++ src/Mainheader.jsx
@@ -3,7 +3,7 @@ export default function Mainheader() {
     <header className="bg-dark text-light ">
       <div className="container d-flex  justify-content-between align-items-center p-1">
         <h1>cybr</h1>
-        <img className="col-3" src="/Search Field (1).png" alt="Search Field" />
+        <img className="col-3" src="/Task-React/Search Field (1).png" alt="Search Field" />
         <nav className=" d-flex gap-3">
           <a>Home</a>
           <a>About</a>
